5b9d91ff82a6aa1eed88c54376a7ad79d4ab0735
LouisCunningham <cunningtechgroup@outlook.com>
2024-01-15 12:36:22 -0500

    75% completed

--- dist/js/main.js
+++ dist/js/main.js
@@ -50,3 +50,64 @@ for (let i = 0, len = revealDelayElements.length; i <len; i++){
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+
+
+// RESUME CODE
+const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el, index) => {
+  el.onclick = () => {
+    const pageTurnId = el.getAttribute('data-page');
+    const pageTurn = document.getElementById(pageTurnId);
+
+    if(pageTurn.classList.contains('turn')){
+      pageTurn.classList.remove('turn');
+      setTimeout(() =>{
+        pageTurn.style.zIndex = 20 - index;
+      }, 500)
+    }
+    else{
+      pageTurn.classList.add('turn');
+      setTimeout(() =>{
+        pageTurn.style.zIndex = 20 + index;
+      }, 500);
+    }
+  }
+})
+
+//contact me button
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.button.contact-me');
+
+contactMeBtn.onclick = () => {
+  pages.forEach((page,index) =>{
+    setTimeout(() =>{
+      page.classList.add('turn');
+
+      setTimeout(() => {
+        page.style.zIndex = 20 + index;
+      }, 500)
+    }, (index + 1) * 200 + 100)
+  })
+}
+
+let totalPages = pages.length;
+let pageNumber = 0;
+
+function reverseIndex() {
+  pageNumber--;
+  if( pageNumber , 0 ) {
+    pageNumber = totalPages - 1;
+  }
+}
+
+const backProfileBtn = document.querySelector('.back-profile');
+
+backProfileBtn.onclick = () => {
+  pages.forEach((_, index) => {
+    setTimeout(() =>{
+      
+    })
+  })
+}
